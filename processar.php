0bd91d427cd5a63db7463d8454a629dcb6aa8072
Marcelo <joao69@aluno.senai-ce.org.br>
2023-12-06 19:25:56 -0300

    adicionando echo

---
+++ processar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oficina O Marcelo</title>
+</head>
+
+<body>
+    <h1>Oficina O Marcelo</h1>
+    <h2>Pedidos</h2>
+    <hr>
+    <fieldset>
+        <legend>Seus Pedidos!!!</legend>
+        <?php
+        echo '<p>Pedido processado!!!!!</p>';
+            ?>
+    </fieldset>
+</body>
+
+</html>
